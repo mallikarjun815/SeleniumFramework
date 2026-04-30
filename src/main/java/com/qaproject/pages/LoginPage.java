ef5e16255f6a3901dad1ed02d6ef8cc38491305b
mallikarjun815 <mallikarjundayyala45@gmail.com>
2026-04-30 11:53:26 +0530

    added LoginPage

---
+++ src/main/java/com/qaproject/pages/LoginPage.java
@@ -0,0 +1,57 @@
+package com.qaproject.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    private WebDriver driver;
+
+    @FindBy(id = "user-name")
+    private WebElement usernameInput;
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(css = "[data-test='error']")
+    private WebElement errorMessage;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterUsername(String username) {
+        usernameInput.clear();
+        usernameInput.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+    public void clickLogin() {
+        loginButton.click();
+    }
+
+    public ProductsPage login(String user, String pass) {
+        enterUsername(user);
+        enterPassword(pass);
+        clickLogin();
+        return new ProductsPage(driver);
+    }
+
+    public String getErrorMessage() {
+        try {
+            return errorMessage.getText();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
